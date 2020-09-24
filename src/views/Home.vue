cbbec9bef69d1b216197cd59f7eed975832a916a
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-24 19:59:52 +0300

    (5 Button & Icons)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <p
-      class="blue"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum culpa iste voluptatem libero nostrum illo error quae quam ipsa porro, eius sequi ea soluta dicta eum, odit saepe exercitationem vitae.</p>
-    <p
-      class="red white--text"
-    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum culpa iste voluptatem libero nostrum illo error quae quam ipsa porro, eius sequi ea soluta dicta eum, odit saepe exercitationem vitae.</p>
-    <p
-      class="pink lighten-4 red--text"
-    >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis officia officiis pariatur sequi, earum, fugit quisquam harum, itaque eveniet debitis vero iure amet aspernatur. Corrupti fugit cupiditate autem enim esse.</p>
-    <p
-      class="pink lighten-4 red--text text--darken-4"
-    >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis officia officiis pariatur sequi, earum, fugit quisquam harum, itaque eveniet debitis vero iure amet aspernatur. Corrupti fugit cupiditate autem enim esse.</p>
-    <h1 class="display-4">Message Display</h1>
-    <h1 class="display-1">Message Display1</h1>
-    <p class="headline">This Is Headling</p>
-    <p class="subheading">This Is A subheading</p>
-    <p class="caption">This Is A Caption</p>
-    <p class="subheading font-weight-bold">This Is A subheading</p>
-    <p class="caption text-lowercase">This Is A Caption</p>
-    <p class="caption text-uppercase">This Is A Caption</p>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn color="pink">click me</v-btn>
+    <v-btn color="pink">click me</v-btn>
+    <!--يضيف shadow-->
+    <v-btn depressed color="pink">click me</v-btn>
+    <!--بحذف الظل الي حوالين الزر shadow-->
+    <v-btn flat color="pink">click me22</v-btn>
+    <!--يضيف shadow-->
+
+    <v-btn depressed class="pink white--text">
+      <v-icon>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn class="ma-2" tile outlined color="success">
+      <v-icon left>mdi-pencil</v-icon>Edit
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email</span>
+      <v-icon right large>mdi-pencil</v-icon>
+    </v-btn>
+
+
+
+    <v-btn depressed small dark color="purple">
+      <v-icon >mdi-heart</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
