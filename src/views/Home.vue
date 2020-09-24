b497c2fa1ce0174b6bee302ca074c1917a47b087
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-24 19:19:09 +0300

    (4 Text & Colours)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+    <p
+      class="blue"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum culpa iste voluptatem libero nostrum illo error quae quam ipsa porro, eius sequi ea soluta dicta eum, odit saepe exercitationem vitae.</p>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum culpa iste voluptatem libero nostrum illo error quae quam ipsa porro, eius sequi ea soluta dicta eum, odit saepe exercitationem vitae.</p>
+    <p
+      class="pink lighten-4 red--text"
+    >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis officia officiis pariatur sequi, earum, fugit quisquam harum, itaque eveniet debitis vero iure amet aspernatur. Corrupti fugit cupiditate autem enim esse.</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis officia officiis pariatur sequi, earum, fugit quisquam harum, itaque eveniet debitis vero iure amet aspernatur. Corrupti fugit cupiditate autem enim esse.</p>
+    <h1 class="display-4">Message Display</h1>
+    <h1 class="display-1">Message Display1</h1>
+    <p class="headline">This Is Headling</p>
+    <p class="subheading">This Is A subheading</p>
+    <p class="caption">This Is A Caption</p>
+    <p class="subheading font-weight-bold">This Is A subheading</p>
+    <p class="caption text-lowercase">This Is A Caption</p>
+    <p class="caption text-uppercase">This Is A Caption</p>
   </div>
 </template>
 
@@ -8,7 +28,6 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
-
-}
+  name: "Home",
+};
 </script>
