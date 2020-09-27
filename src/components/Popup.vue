3bb606800965934b9e75f0ae40b35f252222463a
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-27 16:19:15 +0300

    ( 25 Datepickers)

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -9,7 +9,15 @@
         <v-form class="px-3">
           <v-text-field v-model="title" label="Title" prepend-icon="folder"></v-text-field>
           <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+
+
+            <v-menu >
+                <v-text-field :value="due" slot="activator"  label="Due date" prepend-icon="date_range"></v-text-field>
+                <v-date-picker v-model="due"></v-date-picker>
+            </v-menu>
+
           <v-spacer></v-spacer>
+
           <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
         </v-form>
       </v-card-text>
@@ -18,16 +26,24 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
 export default {
   data() {
     return {
       title: '',
-      content: ''
+      content: '',
+      due: null,
     }
   },
   methods: {
     submit() {
       console.log(this.title, this.content)
+    }
+  },
+  computed: {
+    formattedDate () {
+    //   console.log(this.due)
+      return this.due ? format(this.due, 'Do MMM YYYY') : ''
     }
   }
 }
